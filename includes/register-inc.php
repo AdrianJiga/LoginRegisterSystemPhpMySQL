3cd41c0c31a585a03963d34020bcd5aa87e82ebd
Adrian Jiga <68645821+AdrianJiga@users.noreply.github.com>
2022-01-19 01:55:19 +0200

    register functionality complete

--- includes/register-inc.php
+++ includes/register-inc.php
@@ -20,26 +20,50 @@
 
     } else {
       $sql = "SELECT username FROM users WHERE username = ?";
-      $stmt = mysq_stmt_init($conn);
+      $stmt = mysqli_stmt_init($conn);
       if (!mysqli_stmt_prepare($stmt, $sql)) {
         header("Location: ../register.php?error=sqlerror");
         exit();
       } else {
         mysqli_stmt_bind_param($stmt, "s", $username);
         mysqli_stmt_execute($stmt);
-        mysqli_stmt_store_result();
+        mysqli_stmt_store_result($stmt); //used for fetching data from the database
         $rowCount = mysqli_stmt_num_rows($stmt);
 
         if($rowCount > 0) {
           header("Location: ../register.php?error=usernametaken");
           exit();
         } else {
-          $sql = "INSERT INTO users () VALUES ()";
+
+					//checking if the SQL statement matches
+          $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+          $stmt = mysqli_stmt_init($conn);
+					if (!mysqli_stmt_prepare($stmt, $sql)) {
+		        header("Location: ../register.php?error=sqlerror");
+		        exit();
+		      } else {
+
+						//using the bcrypt algorithm because this is designed to change overtime.
+						//The length of the result from using this identifier can change over time
+						$hashedPass = password_hash($password, PASSWORD_DEFAULT);
+
+						mysqli_stmt_bind_param($stmt, "ss", $username, $hashedPass);
+						/*
+						==> mysqli_stmt_bind_param($stmt, "ss", $username, $password);
+						wrong because password needs to be hashed. This is sensitive information so we do not want to store it in the database
+						*/
+		        mysqli_stmt_execute($stmt);
+						header("Location: ../register.php?success=registered");
+		        exit();
+						//not needed because this is used only if you fetch data from the database
+		        //mysqli_stmt_store_result($stmt);
+		      }
         }
       }
     }
-
-
+		//closing the statement and the database connection
+		mysqli_stmt_close($stmt);
+		mysqli_close($conn);
   }
 
  ?>
